03175e50d3630e0c6ae9d26cb11f940e0e7fd0ef
Cheng Chin Lim <chengchin5227@gmail.com>
2023-03-18 10:44:31 -0500

    setup express app basic and config

---
+++ src/app.ts
@@ -0,0 +1,73 @@
+import { configs } from './config'
+import express, { type Request, type Response } from 'express'
+import cors from 'cors'
+import { connectMongoDb } from './mongo_db/main'
+import { Player } from './mongo_db/player'
+import { getPlayerStatsData } from './third_party/main'
+import { writeDataToLocalStorage } from './utils/main'
+import { writeToS3Bucket } from './aws/main'
+
+connectMongoDb()
+
+const app = express()
+app.use(
+  express.json(),
+  express.urlencoded({ extended: true }),
+  cors({ origin: true })
+)
+
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).send('Express + TypeScript Server')
+})
+
+app.post('/player', async (req: Request, res: Response) => {
+  const player = new Player({
+    firstName: 'Cheng',
+    lastName: 'Lim',
+    rapidApiId: 1,
+    teamId: 1
+  })
+  const created = await player.save()
+  res.status(200).send(created)
+})
+
+app.get('/player', async (req: Request, res: Response) => {
+  const players = await Player.findOne({
+    teamId: 1
+  })
+  res.status(200).send(players)
+})
+
+// app.get('/stats', async (req: Request, res: Response) => {
+//   try {
+//
+//     res.status(200).send('Got the data')
+//   } catch (err) {
+//     res.status(500).send(err)
+//   }
+// })
+
+app.post('/stats', async (req: Request, res: Response) => {
+  try {
+    const season = parseInt(req.query.season as string)
+    const team = parseInt(req.query.team as string)
+    const statsPerGames = await getPlayerStatsData({ season, team })
+    /*
+    * Learned to write data to local utils
+    * and write it to S3 bucket
+    * but it's not necessary for now since the data size is small
+    * */
+    const filePaths = await writeDataToLocalStorage({
+      externalFilePath: `seasons/${season}/teams/${team}.json`,
+      response: statsPerGames
+    })
+    await writeToS3Bucket(filePaths)
+    res.status(200).send('Done')
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+app.listen(configs.port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${configs.port}`)
+})
---
+++ src/config.ts
@@ -0,0 +1,24 @@
+import { config } from 'dotenv'
+config()
+
+export const configs = {
+  port: process.env.PORT ?? 8888,
+  env: process.env.NODE_ENV ?? 'dev',
+  mongoDB: {
+    db: process.env.MONGODB_DB as string,
+    host: process.env.MONGODB_HOST as string,
+    username: process.env.MONGODB_USERNAME as string,
+    password: process.env.MONGODB_PASSWORD as string
+  },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    region: process.env.AWS_REGION as string,
+    s3Bucket: process.env.AWS_S3_BUCKET as string
+  },
+  rapidApi: {
+    nbaDataUrl: process.env.RAPID_API_NBA_DATA_URL as string,
+    host: process.env.RAPID_API_HOST as string,
+    apiKey: process.env.RAPID_API_KEY as string
+  }
+}
