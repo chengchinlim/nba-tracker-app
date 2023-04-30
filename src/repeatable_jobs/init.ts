9b772fe43a0b4c0eb47ae913c16b40af727801ef
Cheng Chin Lim <chengchin5227@gmail.com>
2023-04-30 18:00:27 -0500

    add body params to control cron job start time

--- src/repeatable_jobs/init.ts
+++ src/repeatable_jobs/init.ts
@@ -1,9 +1,22 @@
 import { CronJobQueue } from './main'
 import { getTeamId } from '../services/main'
 
-const getCronSchedule = (index: number): string => {
+export interface THourMin {
+  min: number
+  hour: number
+}
+
+/*
+* utcStartTime is the time
+* at which the first job will start
+* */
+const getCronSchedule = (index: number, utcStartTime?: THourMin): string => {
   let min = 0
   let hour = 0
+  if (utcStartTime !== undefined) {
+    min = utcStartTime.min
+    hour = utcStartTime.hour
+  }
   let tempIndex = index
   while (tempIndex > 0) {
     /*
@@ -11,28 +24,35 @@ const getCronSchedule = (index: number): string => {
     * because one hour has 60 minutes
     * */
     if (min + 5 >= 60) {
-      hour++
       min = 0
+      /*
+      * Resets when it reaches 12am
+      * */
+      if (hour + 1 >= 24) {
+        hour = 0
+      } else {
+        hour++
+      }
     } else {
       min += 5
     }
     tempIndex--
   }
-  return `${min} ${hour} * * * *`
+  return `0 ${min} ${hour} * * *`
 }
 
 const queue = new CronJobQueue(
   'update_stats_queue'
 )
 
-export const createRepeatableJobs = async (): Promise<string[]> => {
+export const createRepeatableJobs = async (utcStartTime?: THourMin): Promise<string[]> => {
   // const jobs = await queue.listJobs()
   // console.log(jobs)
   const results: string[] = []
   for (let i = 0; i < 50; i++) {
     const teamId = await getTeamId(i)
     if (teamId !== null) {
-      const cronSchedule = getCronSchedule(i)
+      const cronSchedule = getCronSchedule(i, utcStartTime)
       const jobId = await queue.addJob({
         teamId,
         cronSchedule
@@ -50,17 +70,18 @@ export const getRepeatableJobs = async (): Promise<string[]> => {
   return await queue.listJobs()
 }
 
-export const removeRepeatableJobs = async (): Promise<string[]> => {
+export const removeRepeatableJobs = async (utcStartTime?: THourMin): Promise<string[]> => {
   const results: string[] = []
   for (let i = 0; i < 50; i++) {
     const teamId = await getTeamId(i)
     if (teamId !== null) {
-      const cronSchedule = getCronSchedule(i)
+      const cronSchedule = getCronSchedule(i, utcStartTime)
+      const jobName = `update_stats_team_${teamId}`
       const isRemoved = await queue.removeJob({
-        jobName: `update_stats_team_${teamId}`,
+        jobName,
         cronSchedule
       })
-      results.push(`isRemoved: ${isRemoved ? 'true' : 'false'} , TeamId: ${teamId}, cronSchedule: ${cronSchedule}`)
+      results.push(`isRemoved: ${isRemoved ? 'true' : 'false'} , JobName: ${jobName}, cronSchedule: ${cronSchedule}`)
     }
   }
   return results
